578a3661af3134df36ac2f5f61d175baed58491c
faatima_abidi <faatima.abidi24@gmail.com>
2024-10-03 16:11:16 -0400

    Postgres changes done to database

--- tests/app_test.py
+++ tests/app_test.py
@@ -17,6 +17,8 @@ def client():
     with app.app_context():
         db.create_all()  # setup
         yield app.test_client()  # tests run here
+        db.session.close()
+        db.session.close_all()
         db.drop_all()  # teardown
 
 
